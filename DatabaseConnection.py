b4cdcef5c43a8adf0cf9972054e7031e5c8a3e4b
Yunoya <sonrasamon15725@gmail.com>
2024-10-10 20:35:17 +0700

    Update database connection

---
+++ DatabaseConnection.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+class db:
+    def __init__(self):
+        self.host = "sql12.freesqldatabase.com"
+        self.user = "sql12736570"
+        self.password = "BDNeWGZ58K"
+        self.database = "sql12736570"
+
+    def test_connection(self):
+            try:
+                connection = mysql.connector.connect(
+                    host = self.host,
+                    user = self.user,
+                    password = self.password,
+                    database = self.database)
+                cursor = connection.cursor()
+                cursor.execute("SELECT 1")
+                result = cursor.fetchone()
+                print(result)
+            except mysql.connector.Error as err:
+                print("AAAAAAAAAAAAAAAAAAAAAAAA")
+
+test = db().test_connection()
